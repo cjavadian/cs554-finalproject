39637e9c9ba01f2fadd09e8855c8f902fec34d4f
Rozy Gupta <rozygupta024@gmail.com>
2019-04-19 13:17:05 -0400

    Made the validations work

--- client_coursereview/src/components/Navbar.js
+++ client_coursereview/src/components/Navbar.js
@@ -1,20 +1,20 @@
 import React, { Component } from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './Navbar.css';
 import Firebase from './Firebase/firebase';
-import {FaUser,FaLock,FaAt,FaAddressBook } from 'react-icons/fa';
+import { FaUser, FaLock, FaAt, FaAddressBook } from 'react-icons/fa';
 class Navbar extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            firstname:'',
-            lastname:'',
+            firstname: '',
+            lastname: '',
             username: '',
-            email:'',
+            email: '',
             password: '',
-            show:false,
-            error :{
-                message:''
+            show: false,
+            error: {
+                message: ''
             }
         };
         this.handleChange = this.handleChange.bind(this);
@@ -23,30 +23,32 @@ class Navbar extends Component {
     }
 
     handleChange(event) {
-        this.setState({[event.target.id]:event.target.value});
+        this.setState({ [event.target.id]: event.target.value });
     }
 
     onLogin(e) {
         e.preventDefault();
-        Firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.password).then((u)=>{
-        
+        Firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.password).then((u) => {
+
         }).catch((error) => {
             console.log(error);
-            this.setState({error})
-          });
-      }
+            this.setState({ error })
+        });
+    }
 
-      onSignup(e){
+    onSignup(e) {
         e.preventDefault();
-        Firebase.auth().createUserWithEmailAndPassword(this.state.email, this.state.password).then((u)=>{
-           
-        }).then((u)=>{console.log(u)
-            this.props.history.push("/about")})
-        .catch((error) => {
-            console.log(error);
-            this.setState({error})
-          })
-      }
+        Firebase.auth().createUserWithEmailAndPassword(this.state.email, this.state.password).then((u) => {
+
+        }).then((u) => {
+            console.log(u)
+            this.props.history.push("/about")
+        })
+            .catch((error) => {
+                console.log(error);
+                this.setState({ error })
+            })
+    }
     render() {
         return (
             <div>
@@ -55,63 +57,65 @@ class Navbar extends Component {
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-                        <Link className="navbar-brand" to="#">Course Review</Link>  
+                        <Link className="navbar-brand" to="#">Course Review</Link>
                         <ul className="navbar-nav mr-auto">
                         </ul>
                         <form className="form-inline my-2 my-lg-0 justify-content-end" >
                             <input className="form-control mr-sm-2" type="text" placeholder="Email" aria-label="Username" id="email" value={this.state.email} onChange={this.handleChange} />
                             <input className="form-control mr-sm-2" type="password" placeholder="Password" aria-label="Password" id="password" value={this.state.password} onChange={this.handleChange} />
                             <button className="btn btn-outline-success my-2 my-sm-0" type="submit" onClick={this.onLogin}>Login</button>
-                            <div class="divider"/>
+                            <div class="divider" />
                             <button className="btn btn-outline-success my-2 my-sm-0" type="submit" name="signup" id="signup"
-                            data-toggle="modal" data-target="#modalRegisterForm" onClick={(event) => {event.preventDefault()}}>SignUp</button>
-                        <div className="modal fade" id="modalRegisterForm" tabIndex="-1" role="dialog"
-                        aria-labelledby="myModalLabel" aria-hidden="true">
-                        <div className="modal-dialog" role="document">
-                            <div className="modal-content">
-                                <div className="modal-header btn-outline-success text-center">
-                                    <h4 className="modal-title w-100 font-weight-bold">Sign up</h4>
-                                    <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                                        <span aria-hidden="true">&times;</span>
-                                    </button>
-                                </div>
-                                <div className="modal-body mx-3">
-                                <form class="needs-validation" novalidate>
-                                    <div className="md-form mb-4">
-
-                                        <FaUser/>
-                                        <input required type ="text" id="firstname" className="form-control validate" placeholder="First Name" value={this.state.firstname} onChange={this.handleChange} />
+                                data-toggle="modal" data-target="#modalRegisterForm" onClick={(event) => { event.preventDefault() }}>SignUp</button>
+                            <div className="modal fade" id="modalRegisterForm" tabIndex="-1" role="dialog"
+                                aria-labelledby="myModalLabel" aria-hidden="true">
+                                <div className="modal-dialog" role="document">
+                                    <div className="modal-content">
+                                        <div className="modal-header btn-outline-success text-center">
+                                            <h4 className="modal-title w-100 font-weight-bold">Sign up</h4>
+                                            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                                <span aria-hidden="true">&times;</span>
+                                            </button>
+                                        </div>
+                                        <div className="modal-body mx-3">
+                                            <div class="registration-form">
+                                                <form>
+                                                    <div class="form-group mb-4">
+                                                        <FaUser />
+                                                        <input required type="text" id="firstname" className="form-control validate" placeholder="First Name" value={this.state.firstname} onChange={this.handleChange} />
+                                                    </div>
+                                                    <div class="form-group mb-4">
+                                                        <FaUser />
+                                                        <input required type="text" id="lastname" className="form-control validate" placeholder="Last Name" value={this.state.lastname} onChange={this.handleChange} />
+                                                    </div>
+                                                    <div class="form-group mb-4">
+                                                        <FaAddressBook />
+                                                        <input required type="text" id="username" className="form-control validate" placeholder="username" value={this.state.username} onChange={this.handleChange} />
+                                                    </div>
+                                                    <div class="form-group mb-4">
+                                                        <FaAt />
+                                                        <input required type="email" id="email" className="form-control validate" placeholder="Email" value={this.state.email} onChange={this.handleChange} />
+                                                    </div>
+                                                    <div class="form-group mb-4">
+                                                        <FaLock />
+                                                        <input required type="password" id="password" className="form-control validate" placeholder="Password" value={this.state.password} onChange={this.handleChange} />
+                                                    </div>
+                                                    <div className="modal-footer d-flex justify-content-center">
+                                                        <div className="form-group mb-4">
+                                                            <button className="btn btn-outline-success" type="submit" onSubmit={this.onSignup}>Sign up</button>
+                                                        </div>
+                                                    </div>
+                                                </form>
+                                            </div>
+                                        </div>
                                     </div>
-                                    <div className="md-form mb-4">
-                                        <FaUser/>
-                                        <input required type="text" id="lastname" className="form-control validate" placeholder="Last Name" value={this.state.lastname} onChange={this.handleChange}/>
-                                    </div>
-                                    <div className="md-form mb-4">
-                                        <FaAddressBook/>
-                                        <input required type="text" id="username" className="form-control validate" placeholder="username" value={this.state.username} onChange={this.handleChange}/>
-                                    </div>
-                                    <div className="md-form mb-4">
-                                        <FaAt/>
-                                        <input required type="email" id="email" className="form-control validate" placeholder="Email" value={this.state.email} onChange={this.handleChange}/>
-                                    </div>
-                                    <div className="md-form mb-4">
-                                        <FaLock/>
-                                        <input required type="password" id="password" className="form-control validate" placeholder="Password" value={this.state.password} onChange={this.handleChange}/>
-                                    </div>
-                                    </form>
-                                </div>
-                                <div className="modal-footer d-flex justify-content-center">
-                                    <button className="btn btn-outline-success" onClick={this.onSignup}>Sign up</button>
                                 </div>
                             </div>
-                        </div>
-                    </div>
                         </form>
-                        
                     </div>
                 </nav>
-                {/* <div id="indexError">{this.state.error.message}</div> */}
-                
+                <div id="indexError">{this.state.error.message}</div>
+
             </div>
 
         );
