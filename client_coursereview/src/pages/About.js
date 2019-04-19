c41b3e4642159ab59073c1127982833ae78e8ca2
Rozy Gupta <rozygupta024@gmail.com>
2019-04-18 20:22:03 -0400

    converted about to Dashboard

--- client_coursereview/src/pages/About.js
+++ client_coursereview/src/pages/About.js
@@ -15,7 +15,7 @@ class About extends Component{
             <div>
                 <Navbar callBackFromParent={this.onLogIn}/>
                 <div className="container">
-                <h2>About Page</h2>
+                <h2>Dashboard</h2>
                 <p>Welcome User on home page. You've successfully logged in!</p>
                 <button class ="btn btn-outline-success my-2 my-sm-0" onClick={this.onLogOut}>LogOut</button>
                 </div>
