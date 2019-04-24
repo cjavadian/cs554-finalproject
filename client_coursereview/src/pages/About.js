ed6424a784e968df7e9bb9f056b9a9c5da9c169a
VidyaMaiya <vid04star@gmail.com>
2019-04-24 13:44:38 -0400

    Added User card component

--- client_coursereview/src/pages/About.js
+++ client_coursereview/src/pages/About.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import LoggedinNavbar from '../components/LoggedinNavbar';
+import { Link } from 'react-router-dom';
 import Footer from '../components/Footer.js';
 class About extends Component{
 
@@ -8,9 +9,23 @@ class About extends Component{
             <div>
                 {/* <Navbar callBackFromParent={this.onLogIn}/> */}
                 <LoggedinNavbar/>
-                <div className="container">
-                <h2>Dashboard</h2>
-                <p>Welcome User on home page. You've successfully logged in!</p>
+                <div className="container mt-3">
+                    <div className="row">
+                        <div className="col-md-4">
+                        <br/>
+                            <div className="card">
+                                <img className="card-img-top" src="..." alt="Card image cap"/>
+                                    <div className="card-body">
+                                        <h5 className="card-title">Card title</h5>
+                                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                        <Link to="#" class="btn btn-primary">Go somewhere</Link>
+                                    </div>
+                            </div>
+                        </div>
+                        <div className="col-8">
+                            2 of 2
+                        </div>
+                    </div>
                 </div>
                 <Footer/>
             </div>
