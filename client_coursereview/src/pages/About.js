c5de447f75177621c645a8bc4240c19cdbd9ec0d
VidyaMaiya <vid04star@gmail.com>
2019-04-24 13:00:10 -0400

    Added logged in nav bar

--- client_coursereview/src/pages/About.js
+++ client_coursereview/src/pages/About.js
@@ -1,23 +1,16 @@
 import React, {Component} from 'react';
-import Navbar from '../components/Navbar.js';
+import LoggedinNavbar from '../components/LoggedinNavbar';
 import Footer from '../components/Footer.js';
-import Firebase from '../components/Firebase/firebase'
 class About extends Component{
-    constructor(props) {
-        super(props);
-        this.onLogOut = this.onLogOut.bind(this);
-    }
-    onLogOut() {
-        Firebase.auth().signOut();
-    }
+
     render(){
         return(
             <div>
-                <Navbar callBackFromParent={this.onLogIn}/>
+                {/* <Navbar callBackFromParent={this.onLogIn}/> */}
+                <LoggedinNavbar/>
                 <div className="container">
                 <h2>Dashboard</h2>
                 <p>Welcome User on home page. You've successfully logged in!</p>
-                <button class ="btn btn-outline-success my-2 my-sm-0" onClick={this.onLogOut}>LogOut</button>
                 </div>
                 <Footer/>
             </div>
