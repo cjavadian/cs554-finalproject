27379b1e1df8da62c140082fda6a1e46f47315d1
Rozy Gupta <rozygupta024@gmail.com>
2019-05-10 11:28:44 -0400

    Added link to courseReview

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -93,7 +93,7 @@ class Navbar extends Component {
             <span className="navbar-toggler-icon" />
           </button>
           <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-            <Link className="navbar-brand" to="#">
+            <Link className="navbar-brand" to="/">
               Course Review
             </Link>
             <ul className="navbar-nav mr-auto" />
