5a65a6946b8b47142413150675f0654a01b4116d
YvesHarrison <yveslee15@gmail.com>
2019-05-09 23:57:40 -0400

    roll back

--- client/src/pages/EditUserProfile.js
+++ client/src/pages/EditUserProfile.js
@@ -1,49 +1,99 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import LoggedinNavbar from "../components/LoggedinNavbar";
+import "./About.css";
+import Firebase from '../components/Firebase/firebase'
+import { graphql, compose } from "react-apollo";
+import { GET_USER } from "../queries/queries";
 
-export class EditUserProfile extends Component {
-    constructor(props) {
-        super(props);
-        this.state={
-            firstname:'',
-            lastname:'',
-            email:'',
-            password:''
-        }
-        this.handleChange = this.handleChange.bind(this);
-        this.handleUpdate = this.handleUpdate.bind(this);
-    }
-    handleChange(event) {
-        this.setState({ [event.target.id]: event.target.value });
-    }
+class EditUserProfile extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstname: "",
+      lastname: "",
+      email: "",
+      password: ""
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleUpdate = this.handleUpdate.bind(this);
+    this.handlePasswordUpdate = this.handlePasswordUpdate.bind(this);
+  }
+  handleChange(event) {
+    this.setState({ [event.target.id]: event.target.value });
+  }
 
-    handleUpdate(event) {
-        
-    }
+  handleUpdate(event) {}
+
+  handlePasswordUpdate(event) {
+    event.preventDefault();
+    Firebase.auth().currentUser.updatePassword(this.state.password);
+    alert("Password updated, please login again")
+  }
+
   render() {
     return (
       <div>
-          <form onSubmit={this.handleUpdate}>
-            <div className="form-group">
+        <LoggedinNavbar />
+        <br />
+        <h5> Edit User's Profile</h5>
+        <form className="form-edit" onSubmit={this.handlePasswordUpdate}>
+          <div className="form-group">
             <label for="firstname">First Name</label>
-            <input type="text" className="form-control" id="firstname" aria-describedby="firstName" placeholder="Enter First name" value={this.state.firstname} onChange={this.handleChange}/>
-            </div>
-            <div className="form-group">
+            <input
+              type="text"
+              minLength={3}
+              className="form-control"
+              id="firstname"
+              aria-describedby="firstName"
+              placeholder="Enter First name"
+              value={this.state.firstname}
+              onChange={this.handleChange}
+            />
+          </div>
+          <div className="form-group">
             <label for="lastname">Last Name</label>
-            <input type="text" className="form-control" id="lastname" aria-describedby="lastName" placeholder="Enter Last name" value={this.state.lastname} onChange={this.handleChange}/>
-            </div>
-            <div class="form-group">
-            <label for="email">Email address</label>
-            <input read-only type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email" />  
-            </div>
-            <div className="form-group">
+            <input
+              type="text"
+              minLength={3}
+              className="form-control"
+              id="lastname"
+              aria-describedby="lastName"
+              placeholder="Enter Last name"
+              value={this.state.lastname}
+              onChange={this.handleChange}
+            />
+          </div>
+          <div class="form-group">
+            <label for="username">User Name</label>
+            <input
+              minLength={3}
+              type="text"
+              class="form-control"
+              id="username"
+              aria-describedby="username"
+              placeholder="Enter Usrname"
+            />
+          </div>
+          <div className="form-group">
             <label for="password">Password</label>
-            <input type="password" className="form-control" id="password" placeholder="Password" value={this.state.password} onChange={this.handleChange}/>
-            </div>
-            <button type="submit" className="btn btn-primary">Submit</button>
+            <input
+              type="password"
+              minLength={6}
+              className="form-control"
+              id="password"
+              placeholder="Enter only if you want to change"
+              value={this.state.password}
+              onChange={this.handleChange}
+            />
+          </div>
+          <button type="submit" className="btn btn-outline-success">
+            Submit
+          </button>
         </form>
       </div>
-    )
+    );
   }
 }
-
-export default EditUserProfile
+export default compose(
+  graphql(GET_USER, {name: GET_USER})
+)(EditUserProfile);
